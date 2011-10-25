7bcaec010a8d288de2598aa08b4366d829ab6a98
fatmatto <cikkense@gmail.com>
2011-10-25 23:10:33 +0300

    Update demo/index.php

--- demo/index.php
+++ demo/index.php
@@ -66,17 +66,6 @@ $fc->dispatch();
 	*	and let's say that e1 is critical while e2 is not. You might want to know everytime
 	*	e1 occurs by emailing a not to your staff:
 	*
-	*	TODO TUTTO CIò NON HA SENSO! ho implementato un setHandler() non statico, significa che gli
-	*	handler li setto nel catch() non capisco che cosa volessi fare :S
-	
-	*	sarebbe più sensato qualcosa del tipo
-	*	class e1 extends qoo/core/Exception
-	*	{
-			protected $_Handlers = array( array('Class1','method1'), 'global_function_name', array('class2','m2') );
-	*	}
-	*	così a priori so cosa fare con questa exception e get handlers mi da _Handlers
-	*
-	*	mi evita di scrivere mille mila catch bla bla bla
 	*/
 	$handler = new \qoo\core\ExceptionHandler();
 	$handler->printHtmlReport($e);
