2dea487ca67144dba64cc29c3f197c8bd7dc50ff
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2012-01-21 22:45:06 +0100

    Revert "Update demo/init.php"
    This reverts commit 45bc490866cc3db3a8b87f3d32093d35236c102f.

--- demo/init.php
+++ demo/init.php
@@ -6,8 +6,7 @@
 	*	Change this directory to whatever you placed the green directory after you downloaded it
 	*
 */
-define("QOO_ROOT",'../'); //Edited by ntrp (works everywhere fatto pirla xD) 
-//Funziona solo soe metti il tuo sito dentro la cartella di qoo, quindi se hai 2 siti in un server devi metterli tutti dentro la cartella di qoo che è impensabile nitro pirla xD
+define("QOO_ROOT",'../'); //Edited by ntrp (works everywhere fatto pirla xD)
 /*
 	*
 	*	Change this directory to whatever you want to place your application directory.
